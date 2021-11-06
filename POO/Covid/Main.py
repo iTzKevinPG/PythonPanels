15f8414d55400144509c6b9d086bce087928d620
Kevin Useche Jimenez <kevin.useche@pragma.com.co>
2021-11-06 13:24:32 -0500

    change num

--- POO/Covid/Main.py
+++ POO/Covid/Main.py
@@ -3,6 +3,6 @@ from LoadData import LoadData
 data = LoadData()
 
 data.loadData('https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv?accessType=DOWNLOAD%22')
-data.firstElements(30)
+data.firstElements(10)
 data.lastElements(15)
 data.dataGroupby((['Sexo', 'Estado']), 'suma')
