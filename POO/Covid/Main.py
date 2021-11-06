ef106b68c07dc3d5bdfe6d689d4a9b0d1b7749d2
Kevin Useche Jimenez <kevin.useche@pragma.com.co>
2021-11-06 13:29:14 -0500

    url setting

--- POO/Covid/Main.py
+++ POO/Covid/Main.py
@@ -2,7 +2,7 @@ from LoadData import LoadData
 
 data = LoadData()
 
-data.loadData('https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv?accessType=DOWNLOAD%22')
+data.loadData('https://www.datos.gov.co/api/views/gt2j-8ykr/rows.csv?accessType=DOWNLOAD')
 data.firstElements(10)
 data.lastElements(15)
 data.dataGroupby((['Sexo', 'Estado']), 'suma')
